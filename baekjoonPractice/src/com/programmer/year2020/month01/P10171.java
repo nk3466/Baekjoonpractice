8eed23863abc643c6c1425c208eb18ae5d0ec0c2
lee nam kyeong <skarod0401@naver.com>
2021-01-01 22:51:39 +0900

    2020.1.1 백준 1문제
    10171번 그대로 결과를 복사하여 가져왔더니 \r\n이 추가 되어서 애먹었다.. 이부분 다시 복습할 필요가 있다.

---
+++ baekjoonPractice/src/com/programmer/year2020/month01/P10171.java
@@ -0,0 +1,14 @@
+package com.programmer.year2020.month01;
+
+public class P10171 {
+
+	public static void main(String[] args) {
+		System.out.println("\\    /\\\n" + 
+				" )  ( ')\n" + 
+				"(  /  )\n" + 
+				" \\(__)|");
+
+
+	}
+
+}
