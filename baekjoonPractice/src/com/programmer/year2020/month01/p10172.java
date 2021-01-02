6ebae539072f209280e939b4ec130a7fc64f842d
lee nam kyeong <skarod0401@naver.com>
2021-01-02 17:28:59 +0900

    2020.1.2 백준 한문제 완료
    10172번 풀이 완료

---
+++ baekjoonPractice/src/com/programmer/year2020/month01/p10172.java
@@ -0,0 +1,13 @@
+package com.programmer.year2020.month01;
+
+public class p10172 {
+
+	public static void main(String[] args) {
+		System.out.println("|\\_/|\n" + 
+				"|q p|   /}\n" + 
+				"( 0 )\"\"\"\\\n" + 
+				"|\"^\"`    |\n" + 
+				"||_/=\\\\__|");
+	}
+
+}
