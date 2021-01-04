4801ff3179197b01d2c6c8c8ed48e5eebbe1f33f
lee nam kyeong <skarod0401@naver.com>
2021-01-04 22:24:51 +0900

    2021.1.4 백준 4문제 풀기
    year2020으로 되어있어서 year2021로 수정
    
    10430,2588,1330,9498 문제 풀기 완료

---
+++ baekjoonPractice/src/com/programmer/year2021/month01/P10718.java
@@ -0,0 +1,12 @@
+package com.programmer.year2021.month01;
+
+public class P10718 {
+
+	public static void main(String[] args) {
+
+		System.out.println("강한친구 대한육군");
+		System.out.println("강한친구 대한육군");
+
+	}
+
+}
