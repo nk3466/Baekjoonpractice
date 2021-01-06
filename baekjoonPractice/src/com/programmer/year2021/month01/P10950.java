ad5ce0bb456122e3943837a4361a4eb26eeb2f71
lee nam kyeong <skarod0401@naver.com>
2021-01-07 00:40:02 +0900

    2021.1.6 백준 2문제 풀이 완료
    2739, 10950 풀이 완료

---
+++ baekjoonPractice/src/com/programmer/year2021/month01/P10950.java
@@ -0,0 +1,20 @@
+package com.programmer.year2021.month01;
+
+import java.util.Scanner;
+
+public class P10950 {
+
+	public static void main(String[] args) {
+
+		
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		
+		for(int i = 0; i < a; i++) {
+			int A = sc.nextInt();
+			int B = sc.nextInt();
+			System.out.println(A + B);
+		}
+	}
+
+}
