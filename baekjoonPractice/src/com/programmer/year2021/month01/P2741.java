ca72736631a0f136cb34ca2aecafcf2ea1889177
lee nam kyeong <skarod0401@naver.com>
2021-01-08 23:11:44 +0900

    2021.1.8 백준 알고리즘 5문제 완료
    2438, 2741, 2742, 11021, 11022번 풀이 완료

---
+++ baekjoonPractice/src/com/programmer/year2021/month01/P2741.java
@@ -0,0 +1,17 @@
+package com.programmer.year2021.month01;
+
+import java.util.Scanner;
+
+public class P2741 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		
+		for(int i = 1; i <= N; i++) {
+			System.out.println(i);
+		}
+	}
+
+}
