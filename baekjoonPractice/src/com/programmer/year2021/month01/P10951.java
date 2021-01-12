4766fbe15bf0033cd4f2d9482b6fe5d9246f2a17
lee nam kyeong <skarod0401@naver.com>
2021-01-12 22:36:51 +0900

    2021.1.12 백준 1문제 완료
    10951번 풀었으나 Integer으로 변환해서 null이면 멈추라는 if 문을 활용했을 때 안된 이유 파악 필요함

---
+++ baekjoonPractice/src/com/programmer/year2021/month01/P10951.java
@@ -0,0 +1,26 @@
+package com.programmer.year2021.month01;
+
+import java.util.Scanner;
+
+public class P10951 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+	
+		
+			while(sc.hasNextInt()) {
+			int a =sc.nextInt();
+			int b = sc.nextInt();
+			
+			System.out.println(a + b);		
+
+				
+
+			}
+		}
+	
+
+}
+
+
