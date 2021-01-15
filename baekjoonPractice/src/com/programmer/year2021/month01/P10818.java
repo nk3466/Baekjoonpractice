ac8edb4cbe4a5df1127ea10b0e0edd63d8e4f702
lee nam kyeong <skarod0401@naver.com>
2021-01-15 21:55:15 +0900

    2021.1.15 백준 1문제 완료
    10818 번 완료

--- baekjoonPractice/src/com/programmer/year2021/month01/P10818.java
+++ baekjoonPractice/src/com/programmer/year2021/month01/P10818.java
@@ -8,21 +8,23 @@ public class P10818 {
 
 		Scanner sc = new Scanner(System.in);
 		int a = sc.nextInt();
-		int min = -1000000;
-		int max = 1000000;
-				
-		int[] iarr1 = new int[a];
+		int min = 1000000;
+		int max = -100000;
+		
+		int[] arr = new int[a];
 		
 		for(int j = 0; j < a; j++) {
-			iarr1[j] = sc.nextInt();
+			arr[j] = sc.nextInt();
 		}
 		
 		for(int i = 0; i < a; i++) {
-			if(iarr1[i] > max) {
-				max = iarr1[i];
-			}
-			if(iarr1[i] < min) {
-				min = iarr1[i];
+			
+			if(arr[i] > max) {
+
+				max = arr[i];
+			}else if(arr[i] < min) {
+				min = arr[i];
+
 			}
 		
 	}
