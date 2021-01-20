9e64d5035dff4e3250d385010d4824fbaa1beaf3
lee nam kyeong <skarod0401@naver.com>
2021-01-20 22:08:35 +0900

    2021.1.20 백준 한문제
    8958더 보완 필요

---
+++ baekjoonPractice/src/com/programmer/year2021/month01/P8958.java
@@ -0,0 +1,27 @@
+package com.programmer.year2021.month01;
+
+import java.util.Scanner;
+
+public class P8958{
+	 public static void main(String[] args){
+	        Scanner sc=new Scanner(System.in);
+	        
+	        int a =sc.nextInt();
+	       String[] q = new String[a];
+	        int count = 0;
+	        int sum = 0;
+	        
+	        for(int i=0; i < a; i++){
+	            q[i] = sc.nextLine();
+	            
+	        }
+        
+        
+	 			}		
+        
+		 }
+
+
+
+      
+        
