2c370d2a18623a97997fd901861da0bb9b3b97e6
lee nam kyeong <skarod0401@naver.com>
2021-01-21 20:51:50 +0900

    2021.1.21 백준 한문제 완료
    1.20 완성 못한 8958번 기존 풀이 보고 해석하며 풀이 완료

--- baekjoonPractice/src/com/programmer/year2021/month01/P8958.java
+++ baekjoonPractice/src/com/programmer/year2021/month01/P8958.java
@@ -7,21 +7,28 @@ public class P8958{
 	        Scanner sc=new Scanner(System.in);
 	        
 	        int a =sc.nextInt();
-	       String[] q = new String[a];
-	        int count = 0;
-	        int sum = 0;
+	        sc.nextLine();
 	        
-	        for(int i=0; i < a; i++){
-	            q[i] = sc.nextLine();
-	            
-	        }
-        
-        
+	       String[] q = new String[a];
+	        
+	  
+	        
+		  for(int i = 0; i < a; i++) {
+			  q[i] = sc.nextLine();
+			  
+			  	int count =0;
+		        int sum = 0;
+			      for(int j = 0; j < q[i].length(); j++) {
+			    	  if(q[i].charAt(j)=='X') {
+			    		  count = 0;
+			    	  }else {
+			    		  count++;
+			    		  sum += count;
+			    	  }
+			      }
+			      System.out.println(sum);
+			      
+		  }
 	 			}		
         
 		 }
-
-
-
-      
-        
