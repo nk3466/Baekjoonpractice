52ad67bb1795916b24aa55cc4257e438053eaae1
lee nam kyeong <skarod0401@naver.com>
2021-01-24 22:39:35 +0900

    2021.1.24 백준 한문제 커밋
    10809번 문제 더 해야함!

---
+++ baekjoonPractice/src/com/programmer/year2021/month01/P10809.java
@@ -0,0 +1,32 @@
+package com.programmer.year2021.month01;
+
+import java.util.Scanner;
+
+public class P10809 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		String abc = sc.nextLine();
+		int arr[] = new int[26];
+		
+		for(int o = 0; o < arr.length; o++) {
+			arr[o] =-1;
+		}
+		
+		for(int i = 0; i < abc.length(); i++){
+			
+			if(abc.charAt(i) >= 97 && abc.charAt(i) <= 122) {
+				
+				if(arr[i] == -1) {
+					arr[((int)abc.charAt(i))-97]=i;
+				}
+			}
+		}
+		for(int j = 0; j < arr.length; j++) {
+			System.out.print(arr[j] + " ");
+		}
+		
+		}
+
+}
