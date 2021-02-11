09f7a0ca1fad24bcdcae6ad4ddc45d6d7202e250
lee nam kyeong <skarod0401@naver.com>
2021-02-11 23:12:28 +0900

    2021.2.11 백준 한문제 완료
    2839번 보완 완료

--- baekjoonPractice/src/com/programmer/year2021/month02/P2839.java
+++ baekjoonPractice/src/com/programmer/year2021/month02/P2839.java
@@ -7,24 +7,42 @@ public class P2839 {
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
-		int a = sc.nextInt();
-		int result = 0;
-		
-		if(a % 3 == 0 && a % 5 != 0) {
-			result = a / 3;
-		} else {
-			
-		
-		result = a / 5;
-		
-		if(a % 5 == 3) {
-			result += 1;
-		}else {
-			result = -1;
-		}
-		}
-		
-		System.out.println(result);
-	}
+		int num = sc.nextInt();
+        
+        if(num % 5 == 0 ) {
+                
+            System.out.println(num/5);
+            return;
+                
+        }else{    
+            
+            int a = num/5;
+                
+            for(int i = a; i>0; i--) {
+                        
+                int temp = num-(i*5);
+                        
+                if(temp%3==0) {
+                            
+                    System.out.println(i+(temp/3));
+                        
+                    return;
+                }
+            }
+                
+        }            
+            
+        if(num%3==0) {
+                    
+            System.out.println(num/3); 
+                                
+        }else {
+                
+            System.out.println(-1);
+                    
+        }
+            
+        return;
 
+}
 }
