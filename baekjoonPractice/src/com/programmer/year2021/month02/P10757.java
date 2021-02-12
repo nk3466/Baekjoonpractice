891d4e2a560a3e7778902a97d33df46b676b8f09
lee nam kyeong <skarod0401@naver.com>
2021-02-12 22:46:52 +0900

    2021.2.12 백준 한문제 완료
    10757 새로운 객체 BigInteger 알게됨!

---
+++ baekjoonPractice/src/com/programmer/year2021/month02/P10757.java
@@ -0,0 +1,19 @@
+package com.programmer.year2021.month02;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class P10757 {
+
+	public static void main(String[] args) {
+
+		
+		Scanner sc = new Scanner(System.in);
+		
+		BigInteger num1 = sc.nextBigInteger();
+		BigInteger num2 = sc.nextBigInteger();
+		
+		System.out.println(num1.add(num2));
+	}
+
+}
