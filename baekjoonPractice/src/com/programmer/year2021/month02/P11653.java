6119f32a0ff6b5595f08c96851d89c1a585122cc
lee nam kyeong <skarod0401@naver.com>
2021-02-17 23:20:37 +0900

    2021.02.17 백준 한문제 보완 완료
    11653번 풀이완료

--- baekjoonPractice/src/com/programmer/year2021/month02/P11653.java
+++ baekjoonPractice/src/com/programmer/year2021/month02/P11653.java
@@ -1,8 +1,5 @@
 package com.programmer.year2021.month02;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
 import java.util.Scanner;
 
 public class P11653 {
@@ -11,39 +8,16 @@ public class P11653 {
 
 		Scanner sc = new Scanner(System.in);
 		int N = sc.nextInt();
-		List<Integer> list = new ArrayList<Integer>();
-		
+				
 		while(N > 1 ) {
-		for(int i = 1; i <= N; i++) {
-			int a = 0;
-			for(int j = 1; j <= i; j++) {
-				if(i % j == 0) {
-					//System.out.println("i = " + i);
-					a++;
+		for(int i = 2; i <= N; i++) {
+			while(N % i == 0) {
+					System.out.println(i);
+					N = N/i;
 				}
-				//System.out.println("a = " + a);
+				
 			}
-			if(a == 2) {
-				if(N % i ==0) {
-					//System.out.println(i);
-					list.add(i);
-					N = N / i;
-				}
-			}
-		  }
-		}
-		
-	
-		list.sort(new Comparator<Integer>() {
-		
-		@Override
-		public int compare(Integer o1, Integer o2) {
-			return o1.intValue() > o2.intValue()? 1:-1;
-		}
-	});
-		for(Integer a : list) {
-			System.out.println(a);
-		}
+		} 
 
 }
 }
