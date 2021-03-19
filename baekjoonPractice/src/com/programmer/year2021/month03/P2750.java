420b4d7ecbff534f9d395e6643cb89a6242e21dd
lee nam kyeong <skarod0401@naver.com>
2021-03-19 09:23:21 +0900

    2021.03.19 백준 한문제 완료
    2750번 완료

---
+++ baekjoonPractice/src/com/programmer/year2021/month03/P2750.java
@@ -0,0 +1,23 @@
+package com.programmer.year2021.month03;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class P2750 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int arr[] = new int[N];
+		
+		for(int i = 0; i < N; i++) {
+			arr[i] = sc.nextInt();
+		}
+		Arrays.sort(arr);
+		for(int i = 0; i <N; i++ ) {
+			System.out.println(arr[i]);
+		}
+	}
+	
+}
