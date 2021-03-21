e52961c049270ec4071e283c876c088fa45bebd3
lee nam kyeong <skarod0401@naver.com>
2021-03-21 23:26:52 +0900

    2021.03.21 백준 한문제
    다시볼 필요가 있음

--- baekjoonPractice/src/com/programmer/year2021/month03/P2589.java
+++ baekjoonPractice/src/com/programmer/year2021/month03/P2589.java
@@ -1,35 +1,94 @@
 package com.programmer.year2021.month03;
 
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
 
 public class P2589 {
 
-	public static Long[] A = new Long[101];
+	static int H, W;
+	static int dx[] = {1, 0, -1, 0};
+	static int dy[] = {0, 1, 0, -1};
 	
 	public static void main(String[] args) {
-
+		
 		Scanner sc = new Scanner(System.in);
+		int M = sc.nextInt();	//보물 지도 세로 크기
+		int W = sc.nextInt();	//보물 지도 가로 크기
 		
-		int T = sc.nextInt();		// 갯수 입력
+		String map[][] = new String[H][W]; // 보물지도
 		
-		A[0] = 0L;
-		A[1] = 1L;
-		A[2] = 1L;
-		A[3] = 1L;
-		
-		for(int i = 0; i < T; i++) {
-			int N = sc.nextInt();
-			System.out.println(function(N));		
+		for(int i = 0; i < H; i++) {
+			String str = sc.next();
+			for(int j = 0; i < W; j++) {
+				map[i][j] = str.substring(j, j+1);
+			}
 		}
 		
-		
-		
+		int answer = 0;
+		for(int i = 0; i < H; i++) {
+			for(int j = 0; j < W; j++) {
+				int map_max = 0;
+				if(map[i][j].equals("L")) {
+					map_max = bfs(i, j, map);
+				}
+				if(map_max > answer) {
+					answer = map_max;
+				}
+			}
+		}
+		System.out.println(answer);
 	}
-
-	public static Long function(int N) {
-		if(A[N] == null) {					//값이 없다면 
-			A[N] = function(N-2) + function(N - 3);	//피보나치 수열에서 나온 함수 
+	
+	private static int bfs(int x, int y, String[][] map) {
+		String copy_map[][] = new String[H][W];
+		//한번에 방문한 지점에 X표시
+		for(int i = 0; i < H; i++) {
+			for(int j = 0; j < W; j++) {
+				copy_map[i][j] = map[i][j];
+			}
 		}
-		return A[N];
+		
+		int time[][] = new int[H][W];//각 지점에 더달하기 까지 걸리는 시간
+		
+		Queue<XY> Q = new LinkedList<XY>();
+		
+		copy_map[x][y] = "X";	//한번 방문 했으므로 x
+		time[x][y] = 0;//시작 지점까지 걸리는 시간은 0
+		
+		while(!Q.isEmpty()) {
+			XY node = Q.poll();
+			
+			for(int i = 0; i < 4; i++) {
+				int nx = node.x + dx[i];
+				int ny = node.y + dy[i];//상하좌우로 한 칸씩 이동
+				
+			if(nx >= 0 && ny >= 0 && nx < H && ny < W && copy_map[nx][ny].equals("L")) {
+				Q.add(new XY(nx, ny));//큐에 저장
+				copy_map[nx][ny] = "x";//방문했으므로 X
+				
+				time[nx][ny] = time[node.x][node.y] + 1;//시간 증가
+			}
+			}
+		}
+		
+		
+	int max = 0;
+	for(int i = 0; i < H; i++) {
+		for(int j = 0; j < W; j++) {
+			if(time[i][j] > max) {
+				max = time[i][j];
+			}
+		}
+	}
+	
+	return max;
 	}
 }
+	class XY{
+		int x; int y;
+		public XY(int x, int y) {
+			this.x = x;
+			this.y = y;
+		}
+	}
