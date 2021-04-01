31a02998bb78e0300322e098b71ebab090e72404
lee nam kyeong <skarod0401@naver.com>
2021-04-01 23:44:05 +0900

    2021.04.01 백준 한문제 완료
    1924 완료

---
+++ baekjoonPractice/src/com/programmer/year2021/month03/implement/P1924.java
@@ -0,0 +1,42 @@
+package com.programmer.year2021.month03.implement;
+
+import java.util.Scanner;
+
+public class P1924 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		int x = sc.nextInt();
+		int y = sc.nextInt();
+		
+		int day = 0;
+		
+		for(int i = 1; i < x; i++) {
+			if(i == 1 || i == 3 || i == 5 || i == 7 || i == 8 || i == 10 || i == 12) {
+				day += 31;
+			}else if(i == 4 || i == 6 || i == 9 || i == 11) {
+				day += 30;
+			}else if(i == 2) {
+				day += 28;
+			}
+		}
+		day += y;
+		if(day % 7 == 1) {
+			System.out.println("MON");
+		}else if(day % 7 == 2) {
+			System.out.println("TUE");
+		}else if(day % 7 == 3) {
+			System.out.println("WED");
+		}else if(day % 7 == 4) {
+			System.out.println("THU");
+		}else if(day % 7 == 5) {
+			System.out.println("FRI");
+		}else if(day % 7 == 6) {
+			System.out.println("SAT");
+		}else if(day % 7 == 0) {
+			System.out.println("SUN");
+		}
+	}
+
+}
