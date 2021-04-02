32d342be21158223ff3813c0498b4185ee304c5a
lee nam kyeong <skarod0401@naver.com>
2021-04-02 22:10:50 +0900

    2021.04.02 백준한문제 완료
    11718번 완료

---
+++ baekjoonPractice/src/com/programmer/year2021/month03/implement/P11718.java
@@ -0,0 +1,18 @@
+package com.programmer.year2021.month03.implement;
+
+import java.util.Scanner;
+
+public class P11718 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		while(sc.hasNextLine()) {
+			String result = sc.nextLine();
+			System.out.println(result);
+		}
+		sc.close();
+	}
+
+}
